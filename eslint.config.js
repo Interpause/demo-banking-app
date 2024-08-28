6e92c0ec049123d8201a49b42dc6479a9cbbddd1
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2024-08-28 16:09:36 +0800

    refactor(app): Move eslint config to monorepo root
    See https://github.com/eslint/eslint/discussions/16960 for more details.

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,46 @@
 import eslint from '@eslint/js'
 import prettierConfig from 'eslint-config-prettier'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
 
-export default [eslint.configs.recommended, prettierConfig]
+export default [
+  // Global ignore list.
+  { ignores: ['app/dist/'] },
+  // Default eslint config shared for all workspaces.
+  eslint.configs.recommended,
+
+  // Eslint config for the app workspace.
+  {
+    files: ['app/**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    settings: { react: { version: '18.3' } },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+
+  // Disable eslint rules that conflict with prettier.
+  prettierConfig,
+]
